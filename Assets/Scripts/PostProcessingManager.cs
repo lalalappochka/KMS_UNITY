f6fa37689c7332b5d4c6a0fdeaab3115db281aa5
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2021-12-05 22:46:45 +0300

    Added post processing

---
+++ Assets/Scripts/PostProcessingManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingManager : MonoBehaviour
+{
+    [SerializeField] private float _changeSpeed = 0.5f;
+    [SerializeField] private PostProcessVolume _volume;
+
+    private DepthOfField _depth;
+    private float _currentDepth;
+
+    private void Awake()
+    {
+        _volume.profile.TryGetSettings(out _depth);
+
+        _currentDepth = _depth.focusDistance.value;
+    }
+
+    private void Update()
+    {
+        float delta = Input.GetKey(KeyCode.E) ? 1f : (Input.GetKey(KeyCode.Q) ? -1f : 0f);
+        if (!Mathf.Approximately(delta, 0f))
+        {
+            _currentDepth = Mathf.Clamp(_currentDepth + delta * Time.deltaTime * _changeSpeed, .1f, 3f);
+            _depth.focusDistance.value = _currentDepth;
+            Debug.Log(_currentDepth);
+        }
+    }
+}
