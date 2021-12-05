55d5f26d78a4a8f4937a0877b3b59135f8580114
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2021-12-05 22:06:42 +0300

    Started adding planet display

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Outline), typeof(Collider))]
+public class Planet : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _planetName;
+    [SerializeField] private string _planetDesc;
+
+    private Outline _outline;
+
+    public string PlanetName => _planetName;
+    public string PlanetDesc => _planetDesc;
+
+    private void Awake()
+    {
+        _outline = GetComponent<Outline>();
+        _outline.enabled = false;
+    }
+
+    public void OnInteractStart()
+    {
+        _outline.enabled = true;
+        PlanetInfoDisplay.Instance.ShowText(PlanetName, PlanetDesc);
+    }
+
+    public void OnInteractStop()
+    {
+        _outline.enabled = false;
+    }
+
+    public void OnClicked()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlanetInfoDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetInfoDisplay : MonoBehaviour
+{
+    public static PlanetInfoDisplay Instance { get; private set; }
+
+    [SerializeField] private Text _info;
+    [SerializeField] private Text _name;
+    [SerializeField] private CanvasGroup _group;
+
+    private Coroutine _canvasAnim;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowText(string name, string info)
+    {
+        _info.text = info;
+        _name.text = name;
+
+        if (_canvasAnim != null)
+        {
+            StopCoroutine(_canvasAnim);
+        }
+        _canvasAnim = StartCoroutine(FadeCoroutine(0.5f));
+    }
+
+    private IEnumerator FadeCoroutine(float duration)
+    {
+        float startTime = Time.time;
+
+        _group.alpha = 0f;
+        while (true)
+        {
+            float alpha = Mathf.Clamp01((startTime - Time.time) / duration);
+
+            _group.alpha = alpha;
+
+            if (alpha >= 1f) break;
+            yield return null;
+        }
+    }
+}
